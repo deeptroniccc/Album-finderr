602b58f27cf861cc4aac879acc29fa6b7c662774
deeptroniccc <souradeepbar42@gmail.com>
2025-09-16 01:14:02 +0530

    secofd commit

--- codedex-api-template/vite.config.js
+++ codedex-api-template/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ensures Vercel serves index.html for all routes
 })
